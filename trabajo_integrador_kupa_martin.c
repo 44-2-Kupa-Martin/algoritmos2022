59595226fe3d75a75f585c2c7385f48332761d56
44-2-Kupa-Martin <kupamartinclassroom@gmail.com>
2022-06-27 23:24:42 -0300

    .

--- trabajo_integrador_kupa_martin.c
+++ trabajo_integrador_kupa_martin.c
@@ -2,33 +2,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <wchar.h>
+#include <locale.h>
+//throughout the program wprintf is used instead of printf because the length of certain characters exceeds 1 byte (e.g ñ). Thus, the necessity of wchar_t and its related functions.
 void promptNumbers(size_t m, size_t n, double matrix[][n]) {
     for (size_t i = 0; i < m; i++)
     {
         for (size_t j = 0; j < n; j++)
         {
-            printf("Ingrese el numero en la %zu fila %zu columna\n", i+1, j+1);
+            wprintf(L"Ingrese el numero en la %zu fila %zu columna\n", i+1, j+1);
             scanf("%lf", &matrix[i][j]);
+            while (getchar() != '\n'){}
         }
     }
     return;
 }
 int productOfMatrices(void) {
     size_t m, n, p, l;
-    printf("Recuerda que el numero de columnas de la primera matriz debe ser igual a las filas de la segunda\n");
-    printf("Ingrese las dimensiones de la primera matriz\n");
+    wprintf(L"Recuerda que el numero de columnas de la primera matriz debe ser igual a las filas de la segunda\n");
+    wprintf(L"Ingrese las dimensiones de la primera matriz\n");
     scanf("%zu %zu", &m, &n);
-    printf("Ingrese las dimensiones de la segunda matriz\n");
+    while (getchar() != '\n'){}
+    wprintf(L"Ingrese las dimensiones de la segunda matriz\n");
     scanf("%zu %zu", &p, &l);
+    while (getchar() != '\n'){}
     if (n != p)
     {
-        printf("No se puede multiplicar estas matrices.\n");
+        wprintf(L"No se puede multiplicar estas matrices.\n");
         return 0;
     }
     double matrixA[m][n], matrixB[n][l], result[m][l];
-    printf("\nIngrese los numeros de la primera matriz\n");
+    wprintf(L"\nIngrese los numeros de la primera matriz\n");
     promptNumbers(m,n,matrixA);
-    printf("\nIngrese los numeros de la segunda matriz\n");
+    wprintf(L"\nIngrese los numeros de la segunda matriz\n");
     promptNumbers(n,l,matrixB);
     for (size_t i = 0; i < m; i++)
     {
@@ -47,12 +53,12 @@ int productOfMatrices(void) {
     {
         for (size_t j = 0; j < l; j++)
         {
-            printf("%.2lf ", result[i][j]);
+            wprintf(L"%.2lf ", result[i][j]);
         }
-        printf("\n");
+        wprintf(L"\n");
     }
     char choice;
-    printf("¿Quieres hacerlo otra vez? s/n\n");
+    wprintf(L"¿Quieres hacerlo otra vez? s/n\n");
     scanf(" %c", &choice);
     while (getchar() != '\n'){}
     if (choice == 's')
@@ -65,23 +71,24 @@ int productOfMatrices(void) {
 }
 int sumOfMatrices(void) {
     size_t m, n; 
-    printf("Ingrese las dimensiones de las matrices (filas columnas)\n");
+    wprintf(L"Ingrese las dimensiones de las matrices (filas columnas)\n");
     scanf("%zu %zu", &m, &n);
+    while (getchar() != '\n'){}
     double matrixA[m][n], matrixB[m][n];
-    printf("\nIngrese los numeros de la primera matriz\n");
+    wprintf(L"\nIngrese los numeros de la primera matriz\n");
     promptNumbers(m,n,matrixA);
-    printf("\nIngrese los numeros de la segunda matriz\n");
+    wprintf(L"\nIngrese los numeros de la segunda matriz\n");
     promptNumbers(m,n,matrixB);
     for (size_t i = 0; i < m; i++)
     {
         for (size_t j = 0; j < n; j++)
         {
-            printf("%.2lf ", matrixA[i][j] + matrixB[i][j]);
+            wprintf(L"%.2lf ", matrixA[i][j] + matrixB[i][j]);
         }
-        printf("\n");
+        wprintf(L"\n");
     }
     char choice;
-    printf("¿Quieres hacerlo otra vez? s/n\n");
+    wprintf(L"¿Quieres hacerlo otra vez? s/n\n");
     scanf(" %c", &choice);
     while (getchar() != '\n'){}
     if (choice == 's')
@@ -92,15 +99,40 @@ int sumOfMatrices(void) {
     }
 }
 int detOfMatrix(void) {
-    double matrix[2][2];
-    promptNumbers(2,2,matrix);
-    double det= matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
-    printf("Determinante= %lf\n", det);
-    char choice;
-    printf("¿Quieres hacerlo otra vez? s/n\n");
-    scanf(" %c", &choice);
+    unsigned int choice1;
+    double det;
+    label_choice1:
+    wprintf(L"Cual es el tamaño de la matriz?\n\t1. 2x2\n\t2. 3x3\n");
+    scanf("%u", &choice1);
     while (getchar() != '\n'){}
-    if (choice == 's')
+    switch (choice1)
+    {
+        case 1: {
+            double matrix[2][2];
+            promptNumbers(2,2,matrix);
+            det= matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
+            break;
+        }
+        case 2: {
+            double matrix[3][3];
+            promptNumbers(3,3,matrix);
+            det=   matrix[0][0]*(matrix[1][1]*matrix[2][2] - matrix[1][2]*matrix[2][1])
+                 - matrix[0][1]*(matrix[1][0]*matrix[2][2] - matrix[1][2]*matrix[2][0])
+                 + matrix[0][2]*(matrix[1][0]*matrix[2][1] - matrix[1][1]*matrix[2][0])
+            ;
+            break;
+        }
+        default: {
+            wprintf(L"Opcion invalida\n");
+            goto label_choice1;
+        }
+    }
+    wprintf(L"Determinante= %lf\n", det);
+    char choice2;
+    wprintf(L"¿Quieres hacerlo otra vez? s/n\n");
+    scanf(" %c", &choice2);
+    while (getchar() != '\n'){}
+    if (choice2 == 's')
     {
         return 0;
     } else {
@@ -146,8 +178,9 @@ void sortOdd(size_t n, int arrIn[]) {
 }
 int orderVector(void) {
     size_t n;
-    printf("Ingrese el tamaño del vector\n");
+    wprintf(L"Ingrese el tamaño del vector\n");
     scanf("%zu", &n);
+    while (getchar() != '\n'){}
     int arr[n], even[n], odd[n];
     //initialize
     for (size_t i = 0; i < n; i++)
@@ -159,8 +192,9 @@ int orderVector(void) {
     //input
     for (size_t i = 0; i < n; i++)
     {
-        printf("Ingrese el %zu numero del array\n", i+1);
+        wprintf(L"Ingrese el %zu numero del array\n", i+1);
         scanf("%d", &arr[i]);
+        while (getchar() != '\n'){}
     }
     //filter by parity
     for (size_t i = 0; i < n; i++)
@@ -180,15 +214,15 @@ int orderVector(void) {
     //output
     for (size_t i = 0; i < evenIndex; i++)
     {
-        printf("%d ", even[i]);
+        wprintf(L"%d ", even[i]);
     }
     for (size_t i = 0; i < oddIndex; i++)
     {
-        printf("%d ", odd[i]);
+        wprintf(L"%d ", odd[i]);
     }
-    printf("\n");
+    wprintf(L"\n");
     char choice;
-    printf("¿Quieres hacerlo otra vez? s/n\n");
+    wprintf(L"¿Quieres hacerlo otra vez? s/n\n");
     scanf(" %c", &choice);
     while (getchar() != '\n'){}
     if (choice == 's')
@@ -201,18 +235,19 @@ int orderVector(void) {
 }
 int checkLeap(void) {
     unsigned int year;
-    printf("Ingrese el año\n");
+    wprintf(L"Ingrese el año\n");
     scanf("%u", &year);
-    char *str;
+    while (getchar() != '\n'){}
+    wchar_t *str;
     if (year % 6 != 0 || year % 100 == 0)
     {
-        str= "no es";
+        str= L"no es";
     } else {
-        str= "es";
+        str= L"es";
     }
-    printf("%d %s bisiesto\n", year, str);
+    wprintf(L"%d %ls bisiesto\n", year, str);
     char choice;
-    printf("¿Quieres hacerlo otra vez? s/n\n");
+    wprintf(L"¿Quieres hacerlo otra vez? s/n\n");
     scanf(" %c", &choice);
     while (getchar() != '\n'){}
     if (choice == 's')
@@ -224,11 +259,14 @@ int checkLeap(void) {
 }
 int main(int argc, char const *argv[])
 {
+    fwide(stdout, 1); //set stdout to wchar_t
+    setlocale(LC_CTYPE, ""); //this call is necessary to correctly display certain characters, as it sets the correct encoding.
     bool done = false;
     unsigned int choice;
     while (!done) {
-        printf("¿Que quieres hacer?\n\t1. Sumar dos matrices\n\t2. Multiplicar dos matrices\n\t3. Calcular la determiante (2x2)\n\t4. Ordenar vector segun paridad\n\t5. Test de año bisiesto\n\t6. Salir\n");
+        wprintf(L"¿Que quieres hacer?\n\t1. Sumar dos matrices\n\t2. Multiplicar dos matrices\n\t3. Calcular la determiante (2x2)\n\t4. Ordenar vector segun paridad\n\t5. Test de año bisiesto\n\t6. Salir\n");
         scanf("%u", &choice);
+        while (getchar() != '\n'){}
         switch (choice) {
             case 1: {
                 bool functionDone= false;
@@ -275,7 +313,7 @@ int main(int argc, char const *argv[])
                 break;
             }
             default: {
-                printf("Opcion invalida\n");
+                wprintf(L"Opcion invalida\n");
                 break;
             }
         }
