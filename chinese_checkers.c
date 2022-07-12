23ea0192d61f2b6e624c7e3b7c04d2862d7e2c48
44-2-Kupa-Martin <kupamartinclassroom@gmail.com>
2022-07-12 12:47:35 -0300

    '.'

--- chinese_checkers.c
+++ chinese_checkers.c
@@ -1,18 +1,52 @@
 #define UP 0
 #define DOWN 1
-#define UP_LEFT 2
-#define UP_RIGHT 3
-#define DOWN_LEFT 4
-#define DOWN_RIGHT 5
+#define LEFT 2
+#define RIGHT 3
+#define UP_LEFT 4
+#define UP_RIGHT 5
+#define DOWN_LEFT 6
+#define DOWN_RIGHT 7
 #include <stdio.h>
 #include <stdlib.h>
-void propagate(int (*ptr_board)[14][14], size_t x, size_t y, int direction, int length, void (*callback)(int *, void *), void *callbackArg) {
-    int (*board)[14]= *ptr_board;
+void propagate(int (*ptr_board)[19][19], size_t x, size_t y, int direction, int length, void (*callback)(int *, void *), void *callbackArg) {
+    int (*board)[15]= *ptr_board;
     switch (direction) {
+        case UP: {
+            for (size_t i= 0; i < length; i++) {
+                for (size_t j= 0; j <= i; j++) {
+                    (*callback)(&board[x-i][y+j], callbackArg);
+                }
+            }
+            break;
+        }
+        case DOWN: {
+            for (size_t i= 0; i < length; i++) {
+                for (size_t j= 0; j <= i; j++) {
+                    (*callback)(&board[x+i][y-j], callbackArg);
+                }
+            }
+            break;
+        }
+        case LEFT: {
+            for (size_t i= 0; i < length; i++) {
+                for (size_t j= 0; j <= length-i; j++) {
+                    (*callback)(&board[x-j+i][y-i], callbackArg);
+                }
+            }
+            break;
+        }
+        case RIGHT: {
+            for (size_t i= 0; i < length; i++) {
+                for (size_t j= 0; j < length-i; j++) {
+                    (*callback)(&board[x+j-i][y+i], callbackArg);
+                }
+            }
+            break;
+        }
         case UP_LEFT: {
             for (size_t i= 0; i < length ; i++) {
                 for (size_t j= 0; j < length-i; j++) {
-                    callback(&board[x-i][y-j], callbackArg);
+                    (*callback)(&board[x-i][y-j], callbackArg);
                 }
             }
             break;
@@ -20,7 +54,7 @@ void propagate(int (*ptr_board)[14][14], size_t x, size_t y, int direction, int 
         case UP_RIGHT: {
             for (size_t i= 0; i < length ; i++) {
                 for (size_t j= 0; j < length-i; j++) {
-                    callback(&board[x-i][y+j], callbackArg);
+                    (*callback)(&board[x-i][y+j], callbackArg);
                 }
             }
             break;
@@ -28,7 +62,7 @@ void propagate(int (*ptr_board)[14][14], size_t x, size_t y, int direction, int 
         case DOWN_LEFT: {
             for (size_t i= 0; i < length ; i++) {
                 for (size_t j= 0; j < length-i; j++) {
-                    callback(&board[x+i][y-j], callbackArg);
+                    (*callback)(&board[x+i][y-j], callbackArg);
                 }
             }
             break;
@@ -36,7 +70,7 @@ void propagate(int (*ptr_board)[14][14], size_t x, size_t y, int direction, int 
         case DOWN_RIGHT: {
             for (size_t i= 0; i < length ; i++) {
                 for (size_t j= 0; j < length-i; j++) {
-                    callback(&board[x+i][y+j], callbackArg);
+                    (*callback)(&board[x+i][y+j], callbackArg);
                 }
             }
             break;
@@ -47,9 +81,28 @@ void propagate(int (*ptr_board)[14][14], size_t x, size_t y, int direction, int 
     }
     return;
 }
-
+void printBoard(int (*ptr_board)[19][19]) {
+    for (size_t i= 0; i < 15; i++) {
+        for (size_t j= 0; j < 15; j++) {
+            printf("%2d ", (*ptr_board)[i][j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+void setNumber(int *ptr_boardNumber, void *ptr_number) {
+    *ptr_boardNumber= *((int *)ptr_number);
+    return;
+}
 int main(int argc, char const *argv[]) {
-    int board[14][14]= {0};
-
+    int board[19][15]= {0};
+    int red= 1, white= 2, yellow= 3, green= 4, black= 5, blue=6;//bruh
+    propagate(&board, 1, 5, DOWN, 4, &setNumber, &red);
+    propagate(&board, 8, 13, UP, 4, &setNumber, &white);
+    propagate(&board, 13, 13, DOWN, 4, &setNumber, &yellow);
+    propagate(&board, 13, 7, UP, 4, &setNumber, &green);
+    propagate(&board, 9, 1, UP_RIGHT, 4, &setNumber, &black);
+    propagate(&board, 5, 1, DOWN_RIGHT, 4, &setNumber, &blue);
+    printBoard(&board);
     return 0;
 }
